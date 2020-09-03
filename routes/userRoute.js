67409da7a53afa4374615ef2f082a0072d828a22
Muthukumar <muthukumarperiyasamy.mk@gmail.com>
2020-09-03 21:47:04 +0530

    limiter for signup

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,9 +1,18 @@
 const router = require("express").Router();
 const passport = require("passport");
+const rateLimit = require("express-rate-limit");
+
+const createAccountLimiter = rateLimit({
+  windowMs: 60 * 60 * 1000, // 1 hour window
+  max: 5, // start blocking after 5 requests
+  message:
+    "Too many accounts created from this IP, please try again after an hour",
+});
+
 
 const userController = require("../controller/user/index");
 
-router.post("/signup", userController.signup);
+router.post("/signup",createAccountLimiter, userController.signup);
 
 router.post("/login", userController.login);
 
